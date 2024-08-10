c689367d35cc02eae78aa6132a21e3404be8df64
Kunal Rane <kunalrane432@gmail.com>
2024-08-09 20:37:39 -0400

    Created  Kafka Producer to Notify the Order Placed Events

---
+++ src/main/java/com/humber/orders/kafka/OrderPlacedEvent.java
@@ -0,0 +1,24 @@
+package com.humber.orders.kafka;
+
+import java.util.List;
+
+
+
+public class OrderPlacedEvent {
+    private Long orderId;
+    private List<OrderItem> orderItems;
+	public Long getOrderId() {
+		return orderId;
+	}
+	public void setOrderId(Long orderId) {
+		this.orderId = orderId;
+	}
+	public List<OrderItem> getOrderItems() {
+		return orderItems;
+	}
+	public void setOrderItems(List<OrderItem> orderItems) {
+		this.orderItems = orderItems;
+	}
+
+    
+}
