17c390e479bbae578395449d6f3f11ea4db30d9e
Kunal Rane <kunalrane432@gmail.com>
2024-08-10 09:27:12 -0400

    Changed the way Producer is configured

--- src/main/java/com/humber/orders/service/OrderService.java
+++ src/main/java/com/humber/orders/service/OrderService.java
@@ -1,35 +1,47 @@
 package com.humber.orders.service;
 
 
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.stereotype.Service;
 
-import com.humber.orders.kafka.KafkaProducer;
+import com.humber.orders.kafka.KafkaProducerService;
 import com.humber.orders.kafka.OrderPlacedEvent;
 import com.humber.orders.model.Order;
+import com.humber.orders.model.OrderItem;
 import com.humber.orders.repository.OrderRepository;
 
 @Service
 public class OrderService {
 
     private final OrderRepository orderRepository;
-    private final KafkaProducer kafkaProducer;
+    private final KafkaProducerService kafkaProducerService;
 
-    public OrderService(OrderRepository orderRepository, KafkaProducer kafkaProducer) {
+    public OrderService(OrderRepository orderRepository, KafkaProducerService kafkaProducerService) {
         this.orderRepository = orderRepository;
-        this.kafkaProducer = kafkaProducer;
+        this.kafkaProducerService = kafkaProducerService;
     }
 
     public Order placeOrder(Order order) {
         Order savedOrder = orderRepository.save(order);
+        Map<Long, Integer> productMap = convertOrderItemsToMap(order.getOrderItems());
+        OrderPlacedEvent event = new OrderPlacedEvent(savedOrder.getId().toString(), productMap);
 
-        // 
-        OrderPlacedEvent event = new OrderPlacedEvent();
-        event.setOrderId(savedOrder.getId());
-        event.setOrderItems(savedOrder.getOrderItems());
-
-        kafkaProducer.publishOrderPlacedEvent(event);
-
+        // Send event
+        kafkaProducerService.sendOrderPlacedEvent(event);
         return savedOrder;
     }
+    
+    private Map<Long, Integer> convertOrderItemsToMap(List<OrderItem> orderItems) {
+        Map<Long, Integer> productMap = new HashMap<>();
+        for (OrderItem item : orderItems) {
+            productMap.put(item.getProductId(), item.getQuantity());
+        }
+        return productMap;
+    }
 }
+
 
